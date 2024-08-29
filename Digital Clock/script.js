218351bfba135d162bdee2ba7af2e709a487afae
Deepak Kumar Patra <145733842+deepak-401@users.noreply.github.com>
2024-08-29 11:50:59 +0530

    Update script.js of Digital Clock Project

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,1 +1,7 @@
+const clock=document.getElementById('clock')
+const hold_time=new Date().toLocaleTimeString();
 
+setInterval(function(){                      //setInterval() is used to repeatedly execute a specified function or code block at a fixed interval of time, measured in milliseconds.
+    const hold_time=new Date()
+    clock.innerHTML=hold_time.toLocaleTimeString();
+},1000)
